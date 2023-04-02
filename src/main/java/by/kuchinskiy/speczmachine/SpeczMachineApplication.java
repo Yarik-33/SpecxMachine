b53fbe6ae32c2df334d0af53e667768dd700753e
Yarik-33 <yarik.kuchinskiy@gmail.com>
2023-04-02 23:03:34 +0300

    Shop

---
+++ src/main/java/by/kuchinskiy/speczmachine/SpeczMachineApplication.java
@@ -0,0 +1,13 @@
+package by.kuchinskiy.speczmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeczMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeczMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/kuchinskiy/speczmachine/SpeczMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package by.kuchinskiy.speczmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeczMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
