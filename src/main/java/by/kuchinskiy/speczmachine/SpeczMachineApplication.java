346a9f467e1c9db437000e7d22d2971a1640ca29
Yarik-33 <yarik.kuchinskiy@gmail.com>
2023-04-17 23:21:04 +0300

    Shop

--- src/main/java/by/kuchinskiy/speczmachine/SpeczMachineApplication.java
+++ src/main/java/by/kuchinskiy/speczmachine/SpeczMachineApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpeczMachineApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpeczMachineApplication.class, args);
 	}
-
 }
