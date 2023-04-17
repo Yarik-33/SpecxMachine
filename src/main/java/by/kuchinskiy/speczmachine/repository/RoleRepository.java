323e7fb02dd32b725b299e50ffbe3e9770eb9ec9
Yarik-33 <yarik.kuchinskiy@gmail.com>
2023-04-17 23:21:04 +0300

    Shop

--- src/main/java/by/kuchinskiy/speczmachine/repository/RoleRepository.java
+++ src/main/java/by/kuchinskiy/speczmachine/repository/RoleRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends CrudRepository<Role,Long> {
-}
+public interface RoleRepository extends CrudRepository<Role,Long> {}
